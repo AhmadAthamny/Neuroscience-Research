8888e1a373f31b427768dcde14a668dec03e3e09
Ahmad Atamna <ahmed@cs.huji.ac.il>
2024-10-30 12:52:22 +0200

    Created an .mgh file

---
+++ python_programs/labels/txt2mgh.py
@@ -0,0 +1,16 @@
+import numpy as np
+import nibabel as nib
+
+# Load the data from the .txt file
+txt_file_path = r'C:\Users\Ahmad Athamny\Documents\University\SummerInternship\Neuroscience-Research\brain_regions\labels\text_files\LH_S1_A1_V1.txt'
+mgh_file_path = r'C:\Users\Ahmad Athamny\Documents\University\SummerInternship\Neuroscience-Research\brain_regions\labels\mgh_files\LH_S1_A1_V1.mgh'
+
+# Assuming the .txt file is organized by vertex with each row as a single value
+data = np.loadtxt(txt_file_path, dtype=np.float32)  # Ensure data is in float32
+
+# Create an identity affine for simplicity
+affine = np.eye(4)
+
+# Save data to .mgh using nibabel
+mgh_image = nib.MGHImage(data.reshape(-1, 1, 1), affine)
+nib.save(mgh_image, mgh_file_path)
