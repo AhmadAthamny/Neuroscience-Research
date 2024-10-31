87a56ab17439b9db94364ee0bdea75663087dc5b
Ahmad Atamna <ahmed@cs.huji.ac.il>
2024-10-31 09:57:42 +0200

    Resampling RH.

--- commands/labels/mgh_resampler.sh
+++ commands/labels/mgh_resampler.sh
@@ -1,6 +1,6 @@
 # Set variables for paths
-FSAVERAGE7_MGH="/home/ahmed/Neuroscience-Research/brain_regions/labels/mgh_files/LH_S1_A1_V1.mgh"  # Path to your fsaverage7 .mgh file
-FSAVERAGE6_MGH="/home/ahmed/Neuroscience-Research/brain_regions/labels/mgh_files/LH_S1_A1_V1_fsavg6.mgh"  # Desired output file in fsaverage6
+FSAVERAGE7_MGH="/home/ahmed/Neuroscience-Research/brain_regions/labels/mgh_files/RH_S1_A1_V1.mgh"  # Path to your fsaverage7 .mgh file
+FSAVERAGE6_MGH="/home/ahmed/Neuroscience-Research/brain_regions/labels/mgh_files/RH_S1_A1_V1_fsavg6.mgh"  # Desired output file in fsaverage6
 
 # Resample from fsaverage7 to fsaverage6
 mri_surf2surf --srcsubject fsaverage --trgsubject fsaverage6 \
