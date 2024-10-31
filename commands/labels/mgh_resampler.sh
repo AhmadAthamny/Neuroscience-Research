38fd8e72756c48cc68784d7e97d25ea8463d67d5
Ahmad Atamna <ahmed@cs.huji.ac.il>
2024-10-31 09:56:43 +0200

    Resampling.

--- commands/labels/mgh_resampler.sh
+++ commands/labels/mgh_resampler.sh
@@ -3,5 +3,6 @@ FSAVERAGE7_MGH="/home/ahmed/Neuroscience-Research/brain_regions/labels/mgh_files
 FSAVERAGE6_MGH="/home/ahmed/Neuroscience-Research/brain_regions/labels/mgh_files/LH_S1_A1_V1_fsavg6.mgh"  # Desired output file in fsaverage6
 
 # Resample from fsaverage7 to fsaverage6
-mri_surf2surf --srcsubject fsaverage7 --trgsubject fsaverage6 \
+mri_surf2surf --srcsubject fsaverage --trgsubject fsaverage6 \
               --hemi lh --sval "$FSAVERAGE7_MGH" --tval "$FSAVERAGE6_MGH"
+
