993ee6b04b59f0474e0ef4c3647b154b92496ce6
Ahmad Atamna <ahmed@cs.huji.ac.il>
2024-10-31 12:22:25 +0200

    new txt files

--- python_programs/labels/mat2text4lbl.py
+++ python_programs/labels/mat2text4lbl.py
@@ -2,7 +2,7 @@ import scipy.io as sio
 import numpy as np
 
 # Load .mat file
-mat_data = sio.loadmat('brain_regions\\surfaceData_matlab\\LH_S1_A1_V1.mat')
+mat_data = sio.loadmat('brain_regions\\surfaceData_matlab\\RH_S1_A1_V1.mat')
 
 # Extract region values from the .mat file (adjust keys as necessary)
 region_data = mat_data["vertices"]  # Replace 'region_data' with actual key
@@ -10,10 +10,10 @@ output_data = np.zeros_like(region_data)
 
 # Define unique values for each region
 # Preserve original values for each region
-output_data[region_data == 7] = 7   # S1 label remains 7
-output_data[region_data == 3] = 3   # A1 label remains 3
-output_data[region_data == 2.5] = 2.5  # V1 label remains 2.5
+output_data[region_data == 7] = 30  # S1 label remains 7
+output_data[region_data == 3] = 20  # A1 label remains 3
+output_data[region_data == 2.5] = 10  # V1 label remains 2.5
 
 
 # Save to text file
-np.savetxt('brain_regions\\labels\\text_files\\LH_S1_A1_V1.txt', output_data, fmt='%d')
+np.savetxt('brain_regions\\labels\\text_files\\RH_S1_A1_V1.txt', output_data, fmt='%d')
