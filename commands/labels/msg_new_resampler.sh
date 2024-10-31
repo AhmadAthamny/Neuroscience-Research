817080e0dd0e2b02180c0fe60060b50bd3f199ed
Ahmad Atamna <ahmed@cs.huji.ac.il>
2024-10-31 12:02:03 +0200

    changes done.

--- commands/labels/msg_new_resampler.sh
+++ commands/labels/msg_new_resampler.sh
@@ -1,8 +1,8 @@
 # Set paths for the input and output files
-FSAVERAGE7_MGH="/home/ahmed/Neuroscience-Research/brain_regions/labels/mgh_files/LH_S1_A1_V1.mgh"  # fsaverage7 .mgh file
-FSAVERAGE6_MGH="/home/ahmed/Neuroscience-Research/brain_regions/labels/mgh_files/LH_S1_A1_V1_fsavg6_nnf.mgh"  # Output file in fsaverage6
+FSAVERAGE7_MGH="/home/ahmed/Neuroscience-Research/brain_regions/labels/mgh_files/RH_S1_A1_V1.mgh"  # fsaverage7 .mgh file
+FSAVERAGE6_MGH="/home/ahmed/Neuroscience-Research/brain_regions/labels/mgh_files/RH_S1_A1_V1_fsavg6_nnf.mgh"  # Output file in fsaverage6
 
 # Use mri_surf2surf with nnf (nearest neighbor forward) mapping method
 mri_surf2surf --srcsubject fsaverage --trgsubject fsaverage6 \
-              --hemi lh --sval "$FSAVERAGE7_MGH" --tval "$FSAVERAGE6_MGH" \
+              --hemi rh --sval "$FSAVERAGE7_MGH" --tval "$FSAVERAGE6_MGH" \
               --mapmethod nnf
